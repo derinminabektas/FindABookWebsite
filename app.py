ce2b4d1b9a63892c32dd33bf408f2568bdd49553
Isik Sezgin <isik.sezgin@std.yeditepe.edu.tr>
2025-12-18 10:28:06 +0300

    ref fake data change

--- app.py
+++ app.py
@@ -8,7 +8,33 @@ def home():
 
 @app.route('/results', methods=['POST'])
 def results():
-    return render_template('results.html')
+    user_books = []
+
+    for i in range(1, 6):
+        book = request.form.get(f'book{i}')
+        if book:
+            user_books.append(book)
+
+    # TEMPORARY backend logic (placeholder)
+    similar_by_tags = [
+        "Dune",
+        "Neuromancer",
+        "Foundation",
+        "Brave New World"
+    ]
+
+    similar_by_users = [
+        "The Hobbit",
+        "Fahrenheit 451",
+        "Snow Crash",
+        "The Left Hand of Darkness"
+    ]
+
+    return render_template(
+        'results.html',
+        similar_by_tags=similar_by_tags,
+        similar_by_users=similar_by_users
+    )
 
 if __name__ == '__main__':
     app.run(debug=True)
